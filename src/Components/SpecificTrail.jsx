538e0357359f04edbf6ea3b40751c49c4b24c1af
AlexSaxena <saxenaalex@gmail.com>
2019-11-20 13:45:25 +0100

    Merges from upstream
     - Refactors code in SpecificTrail

--- src/Components/SpecificTrail.jsx
+++ src/Components/SpecificTrail.jsx
@@ -1,40 +1,62 @@
-import React, { Component } from 'react'
-import { getSpecificTrail } from '../Modules/trailsData'
-import { Container, Grid, Header, Divider, Image } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { getSpecificTrail } from "../Modules/trailsData";
+import {
+  Container,
+  Grid,
+  Header,
+  Divider,
+  Image,
+  Icon
+} from "semantic-ui-react";
+import axios from "axios";
+import { connect } from "react-redux";
 import { Map, GoogleApiWrapper, Marker, Polyline } from 'google-maps-react'
 
 class SpecificTrail extends Component {
   state = {
     trail: null,
     errorMessage: null
-  }
+  };
 
   async componentDidMount() {
-    let response = await getSpecificTrail(this.props.match.params.id)
+    let response = await getSpecificTrail(this.props.match.params.id);
     if (response.error_message) {
       this.setState({
         errorMessage: response.error_message
-      })
+      });
     } else {
       this.setState({
         trail: response
-      })
+      });
     }
   }
 
   goBack = () => {
-    this.props.history.goBack()
-  }
+    this.props.history.goBack();
+  };
+
+  bookMark = async () => {
+    try {
+      await axios.post("http://localhost:3000/v1/bookmarks", {
+        trail_id: this.state.trail.id
+      });
+      this.props.history.push(
+        `/user/${this.props.currentUser.attributes.name}`
+      );
+    } catch (error) {
+      return error.response.data.error_message;
+    }
+  };
 
   render() {
-    let singleTrail, backButton, trailMap
-    const trail = this.state.trail
+    let singleTrail, backButton, trailMap;
+    const trail = this.state.trail;
     const style = {
-      width: '80%',
-      height: '50%',
-      left: '10%',
-      borderRadius: '8px'    
-    }
+      width: "80%",
+      height: "50%",
+      left: "10%",
+      borderRadius: "8px"
+    };
 
     if (trail) {
       const trailCoords = this.state.trail.coordinates.map(trail => ({lat: trail.latitude, lng: trail.longitude}))  
@@ -71,28 +93,35 @@ class SpecificTrail extends Component {
             />
           </Map>
         </>
-      )
+      );
     }
 
     if (trail) {
       singleTrail = (
         <>
-          <Container textAlign='justified' id='specific-trail'>
+          <Icon size="large" name="bookmark" onClick={this.bookMark} />
+          <Container textAlign="justified" id="specific-trail">
             <Grid columns={2}>
               <Grid.Row>
                 <Grid.Column width={5}>
-                  <Image
-                    id={`image_${trail.id}`}
-                    src={trail.image}
-                  />
+                  <Image id={`image_${trail.id}`} src={trail.image} />
                 </Grid.Column>
                 <Grid.Column>
-                  <Header as='h2' id={`title_${trail.id}`}>{trail.title}</Header>
+                  <Header as="h2" id={`title_${trail.id}`}>
+                    {trail.title}
+                  </Header>
                   <Divider />
-                  <p className='single-description' id={`description_${trail.id}`}>{trail.description}</p>
-                  <div className='single-trail'>
+                  <p
+                    className="single-description"
+                    id={`description_${trail.id}`}
+                  >
+                    {trail.description}
+                  </p>
+                  <div className="single-trail">
                     <h3>Good to know:</h3>
-                    <p className='single-content' id={`extra_${trail.id}`}>{trail.extra}</p>
+                    <p className="single-content" id={`extra_${trail.id}`}>
+                      {trail.extra}
+                    </p>
                   </div>
                   <div className='single-trail'>
                     <h3>City:</h3>
@@ -102,33 +131,39 @@ class SpecificTrail extends Component {
                     <h3>Country:</h3>
                     <p className='single-content' id={`country_${trail.id}`}>{trail.country}</p>
                   </div>
-                  <div className='single-trail'>
+                  <div className="single-trail">
                     <h3>Continent:</h3>
-                    <p className='single-content' id={`continent_${trail.id}`}>{trail.continent}</p>
-                  </div> 
-                  <div className='single-trail'>
-                    <h3>Duration:</h3>
-                    <p className='single-content' id={`duration_${trail.id}`}>{trail.duration} min</p>
+                    <p className="single-content" id={`continent_${trail.id}`}>
+                      {trail.continent}
+                    </p>
                   </div>
-                  <div className='single-trail'>
+                  <div className="single-trail">
+                    <h3>Duration:</h3>
+                    <p className="single-content" id={`duration_${trail.id}`}>
+                      {trail.duration} min
+                    </p>
+                  </div>
+                  <div className="single-trail">
                     <h3>Intensity Level:</h3>
-                    <p className='single-content' id={`intensity_${trail.id}`}>{trail.intensity}</p>
+                    <p className="single-content" id={`intensity_${trail.id}`}>
+                      {trail.intensity}
+                    </p>
                   </div>
                 </Grid.Column>
               </Grid.Row>
             </Grid>
           </Container>
         </>
-      )
+      );
     } else {
-      singleTrail = (
-        <h3 id='error-message'>{this.state.errorMessage}</h3>
-      )
+      singleTrail = <h3 id="error-message">{this.state.errorMessage}</h3>;
     }
 
     backButton = (
-      <a id='back-button' onClick={this.goBack} href='#'>Go Back</a>
-    )
+      <a id="back-button" onClick={this.goBack} href="#">
+        Go Back
+      </a>
+    );
 
     return (
       <>
@@ -136,10 +171,18 @@ class SpecificTrail extends Component {
         {backButton}
         {trailMap}
       </>
-    )
+    );
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey:(process.env.REACT_APP_API_KEY)
-})(SpecificTrail)
+const mapStateToProps = state => {
+  return {
+    currentUser: state.reduxTokenAuth.currentUser
+  };
+};
+
+export default connect(mapStateToProps)(
+  GoogleApiWrapper({
+    apiKey: process.env.REACT_APP_API_KEY
+  })(SpecificTrail)
+);
